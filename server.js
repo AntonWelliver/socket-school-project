b577e4ea4dc6bee5eb3c7dec53b624deb8d2df3e
Anton <antonwelliver27@gmail.com>
2019-11-26 10:31:43 +0100

    added functionality to chat

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const io = require("socket.io")(3000);
+const users = [];
 
-app.use(express.static('public'))
+io.on("connection", socket => {
+    console.log("connected");
 
-app.listen(port, () => console.log(`Server started on port ${port}!`))
+    socket.on("new-user", name => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-connected", name);
+    });
+
+    socket.on("send-chat-message", message => {
+        socket.broadcast.emit("chat-message", {
+            message: message,
+            name: users[socket.id]
+        })
+    });
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user-disconnected", users[socket.id]);
+        delete users[socket.id];
+    });
+});
